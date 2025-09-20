e2715660127bea3eda44941fc53feb657f031ae6
Joe <carlucci.j@northeastern.edu>
2025-09-19 22:49:37 -0400

    info added to lab 1 page as outlined in canvas assignment

--- app/Labs/Lab1/page.tsx
+++ app/Labs/Lab1/page.tsx
@@ -9,7 +9,23 @@ export default function Lab1() {
             <h3>HTML Examples</h3>
             <div id="wd-h-tag">
                 <h4>Heading Tags</h4>
-                Text documents are often broken up into several sections and subsections. Each section is usually prefaced with a short title or heading that attempts to summarize the topic of the section it precedes. For instance this paragraph is preceded by the heading Heading Tags. The font of the section headings are usually larger and bolder than their subsection headings. This document uses headings to introduce topics such as HTML Documents, HTML Tags, Heading Tags, etc. HTML heading tags can be used to format plain text so that it renders in a browser as large headings. There are 6 heading tags for different sizes: h1, h2, h3, h4, h5, and h6. Tag h1 is the largest heading and h6 is the smallest heading.
+                <p>
+                    Joseph Carlucci, Section 06<br />
+                    <a href="https://github.com/Joseph-Carlucci/kambaz-next-js">
+                        https://github.com/Joseph-Carlucci/kambaz-next-js
+                    </a>
+                </p>
+                <p>
+                    Text documents are often broken up into several sections and subsections.
+                    Each section is usually prefaced with a short title or heading that attempts
+                    to summarize the topic of the section it precedes. For instance this paragraph
+                    is preceded by the heading Heading Tags. The font of the section headings are
+                    usually larger and bolder than their subsection headings. This document uses
+                    headings to introduce topics such as HTML Documents, HTML Tags, Heading Tags, etc.
+                    HTML heading tags can be used to format plain text so that it renders in a
+                    browser as large headings. There are 6 heading tags for different sizes: h1, h2,
+                    h3, h4, h5, and h6. Tag h1 is the largest heading and h6 is the smallest heading.
+                </p>
             </div>
             <div id="wd-p-tag">
                 <h4>Paragraph Tag</h4>
@@ -144,7 +160,7 @@ export default function Lab1() {
             <div id="wd-images">
                 <h4>Image tag</h4>
                 Loading an image from the internet: <br />
-                <Image id="wd-starship" width={400} height={300} src="https://www.staradvertiser.com/wp-content/uploads/2021/08/web1_Starship-gap2.jpg" alt="Starship rocket" />
+                <img id="wd-starship" width={400} height={300} src="https://www.staradvertiser.com/wp-content/uploads/2021/08/web1_Starship-gap2.jpg" alt="Starship rocket" />
                 <br />
                 Loading a local image:
                 <br />
